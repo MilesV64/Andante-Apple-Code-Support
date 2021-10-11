92f2d60cd8486a54e8bbdd9640a46f506f7be239
MilesV64 <45058355+MilesV64@users.noreply.github.com>
2021-10-10 17:13:01 -0700

    Update DataManager to use NSPersistentHistoryTracking

--- Andante/Model/DataManager.swift
+++ Andante/Model/DataManager.swift
@@ -32,16 +32,22 @@ class DataManager {
         let directory = FileManager.default.urls(
             for: .applicationSupportDirectory, in: .userDomainMask).first!
         
-        //Local data
-        let localStoreDescription = NSPersistentStoreDescription(url: directory.appendingPathComponent("Local.sqlite"))
-        localStoreDescription.configuration = "Local"
+        // Local data
+        let localStoreDescription: NSPersistentStoreDescription = {
+            let description = NSPersistentStoreDescription(url: directory.appendingPathComponent("Local.sqlite"))
+            description.configuration = "Local"
+            return description
+        }()
         
-        //Cloud synced data
-        let cloudStoreDescription = NSPersistentStoreDescription(
-            url: directory.appendingPathComponent("Cloud.sqlite"))
-        cloudStoreDescription.configuration = "Cloud"
-        cloudStoreDescription.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(
-            containerIdentifier: "iCloud.com.milesvinson.andante")
+        // Cloud synced data
+        let cloudStoreDescription: NSPersistentStoreDescription = {
+            let description = NSPersistentStoreDescription(url: directory.appendingPathComponent("Cloud.sqlite"))
+            description.configuration = "Cloud"
+            description.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.com.milesvinson.andante")
+            description.setOption(true as NSNumber, forKey: NSPersistentHistoryTrackingKey)
+            description.setOption(true as NSNumber, forKey: NSPersistentStoreRemoteChangeNotificationPostOptionKey)
+            return description
+        }()
         
         container.persistentStoreDescriptions = [
             cloudStoreDescription,
@@ -56,11 +62,116 @@ class DataManager {
         
         container.viewContext.automaticallyMergesChangesFromParent = true
         container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.transactionAuthor = "Andante"
+        container.viewContext.stalenessInterval = 0
+        
+        NotificationCenter.default.addObserver(
+            self, selector: #selector(self.storeRemoteChange(_:)),
+            name: .NSPersistentStoreRemoteChange, object: container)
         
         return container
     }()
     
+    // MARK: - History tracking
+    // Track the last history token processed for a store, and write its value to file.
+    // The historyQueue reads the token when executing operations, and updates it after processing is complete.
+    private var lastHistoryToken: NSPersistentHistoryToken? = nil {
+        didSet {
+            guard let token = lastHistoryToken,
+                  let data = try? NSKeyedArchiver.archivedData( withRootObject: token, requiringSecureCoding: true)
+            else {
+                return
+            }
+
+            do {
+                try data.write(to: tokenFile)
+            } catch {
+                print("Failed to write token data. Error = \(error)")
+            }
+        }
+    }
+
+    // The file URL for persisting the persistent history token.
+    private lazy var tokenFile: URL = {
+        let url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.milesvinson.Andante")!.appendingPathComponent("CoreDataHistory", isDirectory: true)
+        
+        if !FileManager.default.fileExists(atPath: url.path) {
+            do {
+                try FileManager.default.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
+            } catch {
+                print("Failed to create persistent container URL. Error = \(error)")
+            }
+        }
+        
+        let bundleId = Bundle.main.bundleIdentifier!
+        let tokenURL = url.appendingPathComponent(bundleId + ".token.data", isDirectory: false)
+        print("tokenURL: ", tokenURL)
+
+        return tokenURL
+    }()
+
+
+    // An operation queue for handling history processing tasks: watching changes and triggering UI updates if needed.
+    private lazy var historyQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 1
+        return queue
+    }()
+
+    init() {
+        if let tokenData = try? Data(contentsOf: tokenFile) {
+           do {
+               lastHistoryToken = try NSKeyedUnarchiver.unarchivedObject(ofClass: NSPersistentHistoryToken.self, from: tokenData)
+           } catch {
+               print("Failed to unarchive NSPersistentHistoryToken. Error = \(error)")
+           }
+       }
+   }
+    
 }
+
+// MARK: - Handle Persistent History
+
+extension DataManager {
+    
+    @objc func storeRemoteChange(_ notification: Notification) {
+        // Process persistent history to merge changes from other coordinators.
+        historyQueue.addOperation {
+            self.processPersistentHistory()
+        }
+    }
+    
+    func processPersistentHistory() {
+        let taskContext = self.container.newBackgroundContext()
+        taskContext.performAndWait {
+
+            // Fetch history received from outside the app since the last token
+            let historyFetchRequest = NSPersistentHistoryTransaction.fetchRequest!
+            historyFetchRequest.predicate = NSPredicate(format: "author != %@", "Andante")
+            let request = NSPersistentHistoryChangeRequest.fetchHistory(after: lastHistoryToken)
+            request.fetchRequest = historyFetchRequest
+
+            let result = (try? taskContext.execute(request)) as? NSPersistentHistoryResult
+            guard let transactions = result?.result as? [NSPersistentHistoryTransaction],
+                  !transactions.isEmpty
+            else { return }
+
+            transactions.forEach { transaction in
+                guard let userInfo = transaction.objectIDNotification().userInfo else { return }
+                print("transaction userInfo: ", userInfo)
+                let viewContext = self.container.viewContext
+                NSManagedObjectContext.mergeChanges(fromRemoteContextSave: userInfo, into: [viewContext])
+            }
+
+            // Update the history token using the last transaction.
+            lastHistoryToken = transactions.last!.token
+        }
+    }
+    
+}
+
+
+// MARK: - Helpers
 
 extension DataManager {
     
